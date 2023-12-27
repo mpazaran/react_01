c6e22046b1268e65a0d5826e2579a01e12304165
Mau Pazaran <mauricio.pazaran@gmail.com>
2023-12-27 13:25:01 -0600

    Implementing HandleBars

--- app.js
+++ app.js
@@ -1,38 +1,70 @@
-const port    = 8080
-const fs      = require("fs")
-const express = require('express')
-const app     = express()
+const path     = require('path');
+const fs       = require("fs")
+const express  = require('express')
+const app      = express()
+const {create} = require("express-handlebars");
+const dotenv   = require("dotenv")
+/**
+ * INITIALIZE ENV VARS
+ */
+dotenv.config()
 
-// STATIC CONTENT
+/**
+ * SET HBS ENGINE
+ */
+const hbs = create(
+  {
+    extname: ".hbs"
+  }
+).engine
+app.engine('hbs', hbs);
+app.set('view engine', 'hbs');
+
+/**
+ * DEFINE STATIC CONTENT DIR
+ */
 app.use(express.static("public"))
 
+/**
+ * API ENDPOINTS
+ */
 app.get('/api/hello', function (req, res) {
   res.send('Hello World')
 })
 
 app.get('/api/task/:id', function (req, res) {
   res.send('Here we to list all tasks')
-  console.log(req.params.id)
 })
 
 app.all('api/*', function (req, res) {
   res.send('404 | Page not found')
 })
 
+/**
+ * STATIC CONTENT REDERING
+ */
 app.all('*', function (req, res) {
   try {
-    let target = `${__dirname}/public${req.url}.html`
-    if (fs.existsSync(target)) {
-      res.sendFile(target)
+    let url = req.url.replace(/^\/(.*)(\/*)$/g, "$1")
+    if (url === "") {
+      res.render("home")
     } else {
-      res.sendFile(__dirname + `/public/404.html`)
+      let target = `${__dirname}/views/${url}.hbs`
+      if (fs.existsSync(target)) {
+        res.render(url)
+      } else {
+        res.render("404")
+      }
     }
   } catch (e) {
-    res.sendFile(__dirname + `/public/500.html`)
+    console.log(e.message)
+    res.render("500")
   }
 })
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+/**
+ * START PROCESS
+ */
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port: ${process.env.PORT}`)
 })
-
